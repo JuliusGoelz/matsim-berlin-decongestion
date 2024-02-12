c0980c99ba435a02c4ee07eb58bc5d5cd0bee426
Julius Gölz <juliusgoelz@gmail.com>
2024-02-12 18:03:35 +0100

    add analysis stuff

---
+++ src/main/python/code/run_compute_delays.bat
@@ -0,0 +1,2 @@
+.venv\Scripts\python.exe compute_delays.py "../../../../output/berlin-v6.0-1pct/berlin-v6.0.output_network.xml.gz" "../../../../output/berlin-v6.0-1pct" "../../../../output/berlin-v6.0-1pct-withDecongestion"   "../../../../output/berlin-v6.0-1pct-withRoadpricing"
+pause
